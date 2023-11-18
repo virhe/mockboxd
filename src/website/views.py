9219356f7e87c804875863490c310ceb0e98e61c
tuome <tonni@tuta.io>
2023-11-19 00:03:18 +0200

    Update views.py to work with new templates

--- src/website/views.py
+++ src/website/views.py
@@ -1,18 +1,31 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, redirect, session
 
 views = Blueprint("views", __name__)
 
 
 @views.route("/")
 def index():
-    return render_template("index.html")
+    return render_template("index.html", title="Home")
 
+# AUTH SECTION
 
 @views.route("/signup")
 def signup():
-    return render_template("signup.html")
+    return render_template("signup.html", title="Sign Up")
 
 
 @views.route("/login")
 def login():
-    return render_template("login.html")
+    session["logged_in"] = True
+    return render_template("login.html", title="Log In")
+
+@views.route("/logout")
+def logout():
+    session["logged_in"] = False
+    return redirect("/")
+
+# MAIN SECTION
+
+@views.route("/profile")
+def profile():
+    return render_template("profile.html", title="Profile")
