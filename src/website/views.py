236e8e3e776cc559ed21fb97648456112f64c321
tuome <tonni@tuta.io>
2023-11-19 01:32:13 +0200

    Update signup to work with form

--- src/website/views.py
+++ src/website/views.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, redirect, session
+from .forms.SignupForm import SignupForm
 
 views = Blueprint("views", __name__)
 
@@ -7,11 +8,17 @@ views = Blueprint("views", __name__)
 def index():
     return render_template("index.html", title="Home")
 
+
 # AUTH SECTION
+
 
 @views.route("/signup")
 def signup():
-    return render_template("signup.html", title="Sign Up")
+    signup_form = SignupForm()
+    if signup_form.validate_on_submit():
+        return redirect("/login")
+
+    return render_template("signup.html", title="Sign Up", form=signup_form)
 
 
 @views.route("/login")
@@ -19,12 +26,15 @@ def login():
     session["logged_in"] = True
     return render_template("login.html", title="Log In")
 
+
 @views.route("/logout")
 def logout():
     session["logged_in"] = False
     return redirect("/")
 
+
 # MAIN SECTION
+
 
 @views.route("/profile")
 def profile():
