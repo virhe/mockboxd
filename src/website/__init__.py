0d68cb3d96e44201cdbe48070327c8ab27792825
tuome <tonni@tuta.io>
2023-11-19 04:00:35 +0200

    Fix __init__.py DB creation

--- src/website/__init__.py
+++ src/website/__init__.py
@@ -1,45 +1,30 @@
-import sqlite3
 from flask import Flask
 from flask_wtf.csrf import CSRFProtect
+from dotenv import load_dotenv
+from os import getenv
+from .db import db
 from .views import views
+from .models.user import User
+from .models.movie import Movie
 
 
-def create_db():
-    """Creates needed database tables"""
+def app_startup():
+    """Creates the flask app, as well as the database if it doesn't exist"""
 
-    connection = sqlite3.connect("mockboxd.db")
-    cursor = connection.cursor()
+    load_dotenv()
 
-    users = """ CREATE TABLE IF NOT EXISTS users (
-                id INTEGER PRIMARY KEY,
-                username VARCHAR(30) NOT NULL UNIQUE,
-                password VARCHAR(255) NOT NULL
-                ); """
-
-    cursor.execute(users)
-
-    movies = """ CREATE TABLE IF NOT EXISTS movies (
-                id INTEGER PRIMARY KEY,
-                name VARCHAR(255) NOT NULL,
-                year INTEGER NOT NULL,
-                SCORE INTEGER
-                ); """
-
-    cursor.execute(movies)
-
-    print("DB tables are ready")
-
-    connection.commit()
-    connection.close()
-
-
-def create_flask_app():
-    """Creates flask app"""
-
+    # Flask initialization
     app = Flask(__name__)
-    app.config["SECRET_KEY"] = "TEST"  # THIS SHOULD BE CHANGED IF THE APP IS DEPLOYED!
+    app.config["SECRET_KEY"] = getenv("SECRET_KEY")
+    app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 
     app.register_blueprint(views)
     csrf = CSRFProtect(app)
+
+    # Create database
+    db.init_app(app)
+
+    with app.app_context():
+        db.create_all()
 
     return app
