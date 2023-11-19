71da20419ec7902163260004658ef2dae889c536
tuome <tonni@tuta.io>
2023-11-19 20:09:36 +0200

    Add comments to externals.py

--- src/website/externals.py
+++ src/website/externals.py
@@ -2,6 +2,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager
 
+# Create objects that can be accessed in other modules
 db = SQLAlchemy()
 bcrypt = Bcrypt()
 login_manager = LoginManager()
