eb1ee3953b0f42967c8241a589eea25275a14edb
tuome <tonni@tuta.io>
2023-11-19 19:30:30 +0200

    Refactor signup_form.py

--- src/website/forms/signup_form.py
+++ src/website/forms/signup_form.py
@@ -2,6 +2,7 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, SubmitField
 from wtforms.validators import DataRequired, Length
 
+
 class SignupForm(FlaskForm):
     username = StringField(
         "Username", validators=[DataRequired(), Length(min=4, max=30)]
