0a336b915e1644fd599b42cf17a0be2b6842760c
tuome <tonni@tuta.io>
2023-11-19 20:01:04 +0200

    Fix users.py comment

--- src/website/models/users.py
+++ src/website/models/users.py
@@ -2,7 +2,7 @@ from flask_login import UserMixin
 from ..externals import db
 
 
-# File name in plural due to user being reserved
+# Class name in plural due to user being reserved
 class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
