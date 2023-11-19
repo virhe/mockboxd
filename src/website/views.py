a4de3e86317accffd842ccc3f1116d58051cef84
tuome <tonni@tuta.io>
2023-11-19 06:28:57 +0200

    Implement working signup and login

--- src/website/views.py
+++ src/website/views.py
@@ -1,8 +1,10 @@
 from flask import Blueprint, render_template, redirect, session, flash
+from flask_login import login_user, logout_user, login_required, current_user
 from .externals import bcrypt
-from .forms.SignupForm import SignupForm
+from .forms.signup_form import SignupForm
+from .forms.login_form import LoginForm
 from .externals import db
-from .models.user import User
+from .models.users import User
 
 views = Blueprint("views", __name__)
 
@@ -23,7 +25,7 @@ def signup():
             flash("Username taken!")
             return redirect("/signup")
 
-        hashed_password = bcrypt.generate_password_hash(signup_form.password.data)
+        hashed_password = bcrypt.generate_password_hash(signup_form.password.data).decode("utf-8")
         user = User(username=signup_form.username.data, password=hashed_password)
         db.session.add(user)
         db.session.commit()
@@ -33,15 +35,26 @@ def signup():
     return render_template("signup.html", title="Sign Up", form=signup_form)
 
 
-@views.route("/login")
+@views.route("/login", methods=["GET", "POST"])
 def login():
-    session["logged_in"] = True
-    return render_template("login.html", title="Log In")
+    login_form = LoginForm()
+    if login_form.validate_on_submit():
+        user = User.query.filter((User.username == login_form.username.data)).first()
+
+        if not user or not bcrypt.check_password_hash(user.password, login_form.password.data):
+            flash("Wrong username or password.")
+            return redirect("/login")
+
+        login_user(user)
+
+        return redirect("/profile")
+    
+    return render_template("login.html", title="Log In", form=login_form)
 
 
 @views.route("/logout")
 def logout():
-    session["logged_in"] = False
+    logout_user()
     return redirect("/")
 
 
@@ -49,5 +62,6 @@ def logout():
 
 
 @views.route("/profile")
+@login_required
 def profile():
     return render_template("profile.html", title="Profile")
