3f00554f8ac41aa51f8fcaed086f9a5488f4339b
tuome <tonni@tuta.io>
2023-11-19 06:25:02 +0200

    Rename user.py due to name being reserved

--- src/website/models/users.py
+++ src/website/models/users.py
@@ -1,11 +1,11 @@
 from flask_login import UserMixin
 from ..externals import db
 
-
+# File name in plural due to user being reserved
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
-    password = db.Column(db.String(255), nullable=False)
+    password = db.Column(db.String(60), nullable=False)
 
     def __repr__(self):
         return f"<User {self.username}"
