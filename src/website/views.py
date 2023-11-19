ad8612211bbe7dababe01c9df9dd1452ecdcb737
tuome <tonni@tuta.io>
2023-11-19 04:37:51 +0200

    Implement sign up functionality

--- src/website/views.py
+++ src/website/views.py
@@ -1,5 +1,7 @@
-from flask import Blueprint, render_template, redirect, session
+from flask import Blueprint, render_template, redirect, session, flash
 from .forms.SignupForm import SignupForm
+from .db import db
+from .models.user import User
 
 views = Blueprint("views", __name__)
 
@@ -16,6 +18,14 @@ def index():
 def signup():
     signup_form = SignupForm()
     if signup_form.validate_on_submit():
+        if User.query.filter((User.username == signup_form.username.data)).first():
+            flash("Username taken!")
+            return redirect("/signup")
+
+        user = User(username=signup_form.username.data, password=signup_form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        
         return redirect("/login")
 
     return render_template("signup.html", title="Sign Up", form=signup_form)
