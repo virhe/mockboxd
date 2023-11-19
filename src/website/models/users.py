6507cc0a7e0d057a268741d3296353591a99e65f
tuome <tonni@tuta.io>
2023-11-19 19:55:27 +0200

    Fix users password column size

--- src/website/models/users.py
+++ src/website/models/users.py
@@ -6,7 +6,7 @@ from ..externals import db
 class Users(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
     def __repr__(self):
         return f"<User {self.username}"
