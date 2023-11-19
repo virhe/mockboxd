167aebda3e2acf21736365632928b5c8b20da7f4
tuome <tonni@tuta.io>
2023-11-19 05:22:21 +0200

    Add UserMixin to User model

--- src/website/models/user.py
+++ src/website/models/user.py
@@ -1,7 +1,8 @@
+from flask_login import UserMixin
 from ..externals import db
 
 
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(30), unique=True, nullable=False)
     password = db.Column(db.String(255), nullable=False)
