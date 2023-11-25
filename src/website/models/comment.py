86806fbc89ef9d385875d3067d85943248a6a907
tuome <tonni@tuta.io>
2023-11-25 08:45:05 +0200

    Add comment.py

---
+++ src/website/models/comment.py
@@ -0,0 +1,12 @@
+from ..externals import db
+
+
+class Comment(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+    movie_id = db.Column(db.Integer, db.ForeignKey("movie.id"), nullable=False)
+    comment = db.Column(db.Text, nullable=False)
+    movie = db.relationship("Movie", back_populates="comments")
+
+    def __repr__(self):
+        return f"<Comment - {self.user_id} - {self.movie_id}>"
