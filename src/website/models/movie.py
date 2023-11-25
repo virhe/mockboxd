6900892eec3f7c881971d328cc93f747cb9c1ccd
tuome <tonni@tuta.io>
2023-11-25 08:43:41 +0200

    Update Movie to use new tables

--- src/website/models/movie.py
+++ src/website/models/movie.py
@@ -1,3 +1,4 @@
+from sqlalchemy import func
 from ..externals import db
 
 
@@ -5,7 +6,9 @@ class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     year = db.Column(db.Integer)
-    score = db.Column(db.Integer)
+    date_added = db.Column(db.DateTime(timezone=True), server_default=func.now())
+    ratings = db.relationship("Rating", back_populates="movie")
+    comments = db.relationship("Comment", back_populates="movie")
 
     def __repr__(self):
         return f"<Movie {self.name}"
