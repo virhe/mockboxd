1bbf201da5e44f138b569779686e31b36be6d710
tuome <tonni@tuta.io>
2023-11-25 08:48:11 +0200

    Update views.py to work with new html files

--- src/website/views.py
+++ src/website/views.py
@@ -1,11 +1,16 @@
+import datetime
+
 from flask import Blueprint, render_template, redirect, session, flash
 from flask_login import login_user, logout_user, login_required, current_user
+from sqlalchemy.sql import func
 from .externals import bcrypt
 from .forms.signup_form import SignupForm
 from .forms.login_form import LoginForm
 from .externals import db
 from .models.users import Users
 from .models.movie import Movie
+from .models.comment import Comment
+from .models.rating import Rating
 
 views = Blueprint("views", __name__)
 
@@ -13,8 +18,23 @@ views = Blueprint("views", __name__)
 @views.route("/")
 def index():
     """Handles logic related to the index/home page"""
-    movies = Movie.query.all()
-    return render_template("index.html", title="Home", movies=movies)
+    recently_added = Movie.query.order_by(Movie.date_added).limit(10).all()
+
+    # If no movies have reviews, show recently added instead
+    if Rating.query.first() is not None:
+        top_rated = (
+            Movie.query.join(Rating)
+            .group_by(Movie.id)
+            .order_by(func.avg(Rating.rating))
+            .limit(10)
+            .all()
+        )
+    else:
+        top_rated = recently_added
+
+    return render_template(
+        "index.html", title="Home", top=top_rated, recent=recently_added
+    )
 
 
 # AUTH SECTION
@@ -81,19 +101,28 @@ def profile():
     return render_template("profile.html", title="Profile")
 
 
+@views.route("/movie/<int:movie_id>")
+def movie_info(movie_id):
+    movie = Movie.query.get(movie_id)
+    return render_template("movie_info.html", movie=movie)
+
+
 # TEST SECTION
 
 
 @views.route("/create_movies")
 def create_movies():
     """Temporary view for creating example movies"""
-    test1 = Movie(name="Test1", year=1962, score=4)
-    test2 = Movie(name="Test2", year=1999, score=2)
-    test3 = Movie(name="Test3", year=2012, score=5)
+    test1 = Movie(name="Test1", year=1962)
+    test2 = Movie(name="Test2", year=1999)
+    test3 = Movie(name="Test3", year=2012)
+
+    rating = Rating(user_id=1, movie_id=4, rating=8)
 
     db.session.add(test1)
     db.session.add(test2)
     db.session.add(test3)
+    # db.session.add(rating)
     db.session.commit()
 
     return redirect("/")
@@ -102,7 +131,11 @@ def create_movies():
 @views.route("/delete_movies")
 def delete_movies():
     """Temporary view for deleting example movies"""
-    db.session.query(Movie).delete()
+    movies = Movie.query.all()
+    for movie in movies:
+        Rating.query.filter_by(movie_id=movie.id).delete()
+        Comment.query.filter_by(movie_id=movie.id).delete()
+        db.session.delete(movie)
     db.session.commit()
 
     return redirect("/")
