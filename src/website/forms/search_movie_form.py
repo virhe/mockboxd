6ba18897520f69412522c13e23f44f8a17d9c60c
tuome <tonni@tuta.io>
2023-12-03 15:43:32 +0200

    Remove DataRequired from search_movie_form.py

--- src/website/forms/search_movie_form.py
+++ src/website/forms/search_movie_form.py
@@ -2,11 +2,11 @@
 
 from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms.validators import Length
 
 
 class SearchMovieForm(FlaskForm):
     """Form used for searching for a movie by name"""
 
-    name = StringField("Name", validators=[DataRequired(), Length(max=255)])
+    name = StringField("Name", validators=[Length(max=255)])
     submit = SubmitField("Search")
