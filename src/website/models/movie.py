4c71e07cb247697af9497826c6cb6badf4f90344
tuome <tonni@tuta.io>
2023-12-03 15:03:01 +0200

    Add description and genre to movie.py

--- src/website/models/movie.py
+++ src/website/models/movie.py
@@ -12,6 +12,8 @@ class Movie(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     year = db.Column(db.Integer)
+    description = db.Column(db.Text, nullable=False)
+    genre = db.Column(db.String(255), nullable=False)
     date_added = db.Column(db.DateTime(timezone=True), server_default=func.now())
     ratings = db.relationship("Rating", back_populates="movie")
     comments = db.relationship("Comment", back_populates="movie")
