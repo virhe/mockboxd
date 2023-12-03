b5d9229dde355ae7318ddac99c00ddcf1682a935
tuome <tonni@tuta.io>
2023-12-03 13:04:30 +0200

    Fix schema.sql

--- schema.sql
+++ schema.sql
@@ -38,7 +38,7 @@ CREATE TABLE watchlist (
 CREATE TABLE follower (
     id SERIAL PRIMARY KEY,
     follower_id INTEGER REFERENCES users(id) NOT NULL,
-    followed_id INTEGER REFERENCES movie(id) NOT NULL
+    followed_id INTEGER REFERENCES users(id) NOT NULL
 );
 
 INSERT INTO users (username, password, admin) VALUES ('admin', '$2b$12$blid7Gw50K3ohaHWTcICIuD2G6KJy9HQBRjXKEjcwznvJsIOgTjbi', TRUE);
